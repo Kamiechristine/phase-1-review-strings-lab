67b49fa30bf3dd7af79628b5a9ed6440032a08dc
Kamiechristine <christinemaina5141@gmail.com>
2024-11-26 00:11:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Christine';
+
+const welcomeMessage = "Welcome to Flatbook, $ 'Christine'!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, $ 'CHRISTINE'!";
+
+const shortGreeting = "Welcome, 'C'!";
+
